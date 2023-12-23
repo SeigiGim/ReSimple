64b93cdede35aed7249191ca2db0fdef73386fc4
Seigi Gim <80078548+SeigiGim@users.noreply.github.com>
2023-12-23 01:14:38 -0300

    Finalizando estilos

--- src/components/area/Area.jsx
+++ src/components/area/Area.jsx
@@ -1,7 +1,16 @@
 /* eslint-disable react/prop-types */
 import { useTheme } from "@mui/material/styles";
 import Employee from "../employee/Employee";
-import { Card, CardContent, Typography, Grid } from "@mui/material";
+import {
+	Card,
+	CardContent,
+	Typography,
+	Grid,
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const Area = ({ area, employees }) => {
 	const theme = useTheme();
@@ -10,16 +19,24 @@ const Area = ({ area, employees }) => {
 		<Grid item xs={12} sm={6} sx={{ width: "100%" }}>
 			<Card sx={{ backgroundColor: theme.palette.background.paper }}>
 				<CardContent>
-					<Typography variant="h4" sx={{ color: theme.palette.text.primary }}>
-						{area.NOMBRE_AREA}
-					</Typography>
-					{employees.length === 0 ? (
-						<Typography sx={{ color: theme.palette.text.secondary }}>
-							Sin empleados
-						</Typography>
-					) : (
-						<Employee employees={employees} />
-					)}
+					<Accordion>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+							<Typography
+								variant="h4"
+								sx={{ color: theme.palette.text.primary }}>
+								{area.NOMBRE_AREA}
+							</Typography>
+						</AccordionSummary>
+						<AccordionDetails>
+							{employees.length === 0 ? (
+								<Typography sx={{ color: theme.palette.text.secondary }}>
+									Sin empleados
+								</Typography>
+							) : (
+								<Employee employees={employees} />
+							)}
+						</AccordionDetails>
+					</Accordion>
 				</CardContent>
 			</Card>
 		</Grid>
