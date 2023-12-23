2eec7f88a9f34c2e5603da8c88878b4677354076
Seigi Gim <80078548+SeigiGim@users.noreply.github.com>
2023-12-22 23:22:05 -0300

    validaciones si no hay empleados en un area

--- src/components/area/Area.jsx
+++ src/components/area/Area.jsx
@@ -1,11 +1,17 @@
 /* eslint-disable react/prop-types */
 import Employee from "../employee/Employee";
 
-const Area = ({ area, employees }) => (
-	<div key={area.ID_AREA}>
-		<h3>{area.NOMBRE_AREA}</h3>
-		<Employee employees={employees} />
-	</div>
-);
+const Area = ({ area, employees }) => {
+	return (
+		<div key={area.ID_AREA}>
+			<h3>{area.NOMBRE_AREA}</h3>
+			{employees.length === 0 ? (
+				<p>Sin empleados</p>
+			) : (
+				<Employee employees={employees} />
+			)}
+		</div>
+	);
+};
 
 export default Area;
