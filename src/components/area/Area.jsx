a8dd996f6286c0795c844b40e31cfc83ab24adf2
Seigi Gim <80078548+SeigiGim@users.noreply.github.com>
2023-12-22 23:44:12 -0300

    agregando estilos con MaterialUI

--- src/components/area/Area.jsx
+++ src/components/area/Area.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable react/prop-types */
 import Employee from "../employee/Employee";
+import { Card, CardContent, Typography, Grid } from "@mui/material";
 
 const Area = ({ area, employees }) => {
 	return (
-		<div key={area.ID_AREA}>
-			<h3>{area.NOMBRE_AREA}</h3>
-			{employees.length === 0 ? (
-				<p>Sin empleados</p>
-			) : (
-				<Employee employees={employees} />
-			)}
-		</div>
+		<Grid item xs={12} sm={6}>
+			<Card>
+				<CardContent>
+					<Typography variant="h3">{area.NOMBRE_AREA}</Typography>
+					{employees.length === 0 ? (
+						<Typography>Sin empleados</Typography>
+					) : (
+						<Employee employees={employees} />
+					)}
+				</CardContent>
+			</Card>
+		</Grid>
 	);
 };
 
--- src/components/enterprise/Enterprise.jsx
+++ src/components/enterprise/Enterprise.jsx
@@ -1,19 +1,42 @@
 /* eslint-disable react/prop-types */
+import {
+	Card,
+	CardContent,
+	Typography,
+	Grid,
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Area from "../area/Area";
 
+// En tu componente Enterprise
 const Enterprise = ({ enterprise, employees }) => (
-	<div key={enterprise.ID_EMPRESA}>
-		<h2>{enterprise.NOMBRE_EMPRESA}</h2>
-		{enterprise.AREAS.map((area) => (
-			<Area
-				key={area.ID_AREA}
-				area={area}
-				employees={employees.filter(
-					(employee) => employee.ID_AREA === area.ID_AREA
-				)}
-			/>
-		))}
-	</div>
+	<Grid item xs={12}>
+		<Card>
+			<CardContent>
+				<Accordion>
+					<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+						<Typography variant="h2">{enterprise.NOMBRE_EMPRESA}</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<Grid container spacing={2}>
+							{enterprise.AREAS.map((area) => (
+								<Area
+									key={area.ID_AREA}
+									area={area}
+									employees={employees.filter(
+										(employee) => employee.ID_AREA === area.ID_AREA
+									)}
+								/>
+							))}
+						</Grid>
+					</AccordionDetails>
+				</Accordion>
+			</CardContent>
+		</Card>
+	</Grid>
 );
 
 export default Enterprise;
